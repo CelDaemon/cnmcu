3e8ab1faabf6392374e6ab5f8c5b90f49a07b4d9
CelDaemon <celdaemon@voidgroup.net>
2026-01-08 19:24:58 +0100

    Add item drops and keep code in dropped item

---
+++ src/main/java/com/elmfer/cnmcu/DataComponents.java
@@ -0,0 +1,27 @@
+package com.elmfer.cnmcu;
+
+import com.elmfer.cnmcu.mcu.NanoMCU;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+
+import java.util.function.UnaryOperator;
+
+public class DataComponents {
+    public static final DataComponentType<String> CODE = register("code", builder -> builder.persistent(Codec.STRING).cacheEncoding());
+    public static final DataComponentType<NanoMCU.State> MCU = register("mcu", builder -> builder.persistent(NanoMCU.State.CODEC).cacheEncoding());
+    public static <T> DataComponentType<T> register(String name, UnaryOperator<DataComponentType.Builder<T>> componentFactory) {
+        var componentKey = ResourceKey.create(BuiltInRegistries.DATA_COMPONENT_TYPE.key(), CodeNodeMicrocontrollers.id(name));
+
+        var componentType = componentFactory.apply(DataComponentType.builder()).build();
+        Registry.register(BuiltInRegistries.DATA_COMPONENT_TYPE, componentKey, componentType);
+
+        return componentType;
+    }
+
+    public static void init() {
+        // DUMMY
+    }
+}
--- src/main/java/com/elmfer/cnmcu/blocks/Blocks.java
+++ src/main/java/com/elmfer/cnmcu/blocks/Blocks.java
@@ -1,17 +1,22 @@
 package com.elmfer.cnmcu.blocks;
 
 import com.elmfer.cnmcu.CodeNodeMicrocontrollers;
-import java.util.function.Function;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.PushReaction;
+
+import java.util.function.Function;
 
 public class Blocks {
-    public static final CNnanoBlock CN_NANO_BLOCK = register("nano", CNnanoBlock::new, BlockBehaviour.Properties.of().strength(0.5f));
+    public static final CNnanoBlock CN_NANO_BLOCK = register("nano", CNnanoBlock::new,
+            BlockBehaviour.Properties.of().instabreak().sound(SoundType.STONE).pushReaction(PushReaction.DESTROY)
+    );
 
     public static <T extends Block> T register(String name, Function<BlockBehaviour.Properties, T> blockFactory, BlockBehaviour.Properties blockSettings) {
         var blockKey = ResourceKey.create(BuiltInRegistries.BLOCK.key(), CodeNodeMicrocontrollers.id(name));
