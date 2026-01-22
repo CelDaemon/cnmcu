44bb82f78a92dff448f8bcc431235e9859f1e29e
CelDaemon <celdaemon@voidgroup.net>
2026-01-22 13:26:07 +0100

    Rename mixin to Mojang map

--- src/main/java/com/elmfer/cnmcu/mixins/GuiGraphicsAccessor.java
+++ src/main/java/com/elmfer/cnmcu/mixins/GuiGraphicsAccessor.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(GuiGraphics.class)
-public interface GuiContextInvoker {
+public interface GuiGraphicsAccessor {
     @Invoker("submitBlit")
     void cnmcu$submitBlit(RenderPipeline pipeline, GpuTextureView texture, GpuSampler sampler, int x1, int y1, int x2, int y2, float u1, float v1, float u2, float v2, int color);
 }
